a893d63428c05c25ee3621e02ac06feafde80590
LAPTOP\hyq <hyq961206@sjtu.edu.cn>
2021-03-18 04:31:53 +0900

    linked list reversion!(get declaration of "next" in loop)

---
+++ linked list/[206]Reverse Linked List.java
@@ -0,0 +1,66 @@
+//Given the head of a singly linked list, reverse the list, and return the rever
+//sed list.
+//
+//
+// Example 1:
+//
+//
+//Input: head = [1,2,3,4,5]
+//Output: [5,4,3,2,1]
+//
+//
+// Example 2:
+//
+//
+//Input: head = [1,2]
+//Output: [2,1]
+//
+//
+// Example 3:
+//
+//
+//Input: head = []
+//Output: []
+//
+//
+//
+// Constraints:
+//
+//
+// The number of nodes in the list is the range [0, 5000].
+// -5000 <= Node.val <= 5000
+//
+//
+//
+// Follow up: A linked list can be reversed either iteratively or recursively. C
+//ould you implement both?
+// Related Topics 链表
+// 👍 1584 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode cur = head;
+        while(cur != null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
